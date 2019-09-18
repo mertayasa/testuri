76dff1cd311c442647d4ec7ab11a4f83f04d6d75
Rhap | Ngurah Ady <50355552+Eskies@users.noreply.github.com>
2019-09-18 09:26:44 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/fasthttp/router"
+	"github.com/valyala/fasthttp"
+)
+
+func Index(ctx *fasthttp.RequestCtx) {
+	ctx.WriteString("Welcome to Api!")
+}
+
+func Cetak(next fasthttp.RequestHandler) fasthttp.RequestHandler {
+	return func(ctx *fasthttp.RequestCtx) {
+		next(ctx)
+		ctx.WriteString(fmt.Sprint("<br />Accessed from: ", ctx.RequestURI))
+	}
+}
+
+func main() {
+	r := router.New()
+	r.GET("/", Index)
+
+	log.Fatal(fasthttp.ListenAndServe(":6600", Cetak(r.Handler)))
+}
---
+++ vendor/github.com/fasthttp/router/go.mod
@@ -0,0 +1,9 @@
+module github.com/fasthttp/router
+
+go 1.13
+
+require (
+	github.com/savsgio/gotils v0.0.0-20190916070828-81aa83d987e6
+	github.com/valyala/bytebufferpool v1.0.0
+	github.com/valyala/fasthttp v1.5.0
+)
